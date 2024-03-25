dbc6bd536595678e43d5d6504df933dc75c7ba8c
egor.gututui <egor.gututui@CP-SIO-W11-PG03.bts.sio>
2024-03-25 08:25:47 +0100

    suppression fichiers non necessaires

--- migrations/Version20240325071925.php
+++ migrations/Version20240325071925.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240316205929 extends AbstractMigration
+final class Version20240325071925 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240316205929 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE inscription ADD status VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE inscription ADD date_validation DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE inscription DROP status');
+        $this->addSql('ALTER TABLE inscription DROP date_validation');
     }
 }
--- src/Form/DemandeInscriptionType.php
+++ src/Form/DemandeInscriptionType.php
@@ -23,9 +23,9 @@ class DemandeInscriptionType extends AbstractType {
                     'required' => false,
                 ])
                 ->add('ateliers', EntityType::class, [
-                    'class' => Atelier::class, // Entité Atelier
-                    'choice_label' => 'libelle', // Remplacez 'nom' par le champ que vous souhaitez afficher dans la liste déroulante
-                    'multiple' => true, // Si vous voulez permettre la sélection de plusieurs ateliers
+                    'class' => Atelier::class,
+                    'choice_label' => 'libelle', 
+                    'multiple' => true, 
                     'expanded' => true,
                     'query_builder' => function (EntityRepository $er) {
                         return $er->createQueryBuilder('a')
