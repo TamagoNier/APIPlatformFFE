3d167545cc710e5ff3a40667cd4ecde934569e5b
Egor Gututui <102786178+TamagoNier@users.noreply.github.com>
2024-04-01 18:05:31 +0200

    affichage ateliers

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Atelier;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController {
 
     #[Route('/home', name: 'app_home')]
-    public function index(): Response {
+    public function index(EntityManagerInterface $em): Response {
         
-        return $this->render('/home/index.html.twig');
+        $ateliers = $em->getRepository(Atelier::class)->findAll();
+        return $this->render('/home/index.html.twig', [
+            'ateliers' => $ateliers
+        ]);
     }
 }
